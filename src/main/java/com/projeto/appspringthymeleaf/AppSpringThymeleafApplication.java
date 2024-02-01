9b395fdb6fee2c160bda31d382d42cbb7fc84ed5
Marco Botelho <marcobotelhoabreu@gmail.com>
2024-02-01 20:22:11 -0300

    Commit Inicial

---
+++ src/main/java/com/projeto/appspringthymeleaf/AppSpringThymeleafApplication.java
@@ -0,0 +1,15 @@
+package com.projeto.appspringthymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@SpringBootApplication
+@EnableWebMvc
+public class AppSpringThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppSpringThymeleafApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projeto/appspringthymeleaf/AppSpringThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.appspringthymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppSpringThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
