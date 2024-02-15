1ab87707fde5e749839341ee6b6629288762cc86
Marco Botelho <marcobotelhoabreu@gmail.com>
2024-02-15 16:42:01 -0300

    Inclusão senha usuario criptografada

--- src/main/java/com/projeto/appspringthymeleaf/util/DataInitializer.java
+++ src/main/java/com/projeto/appspringthymeleaf/util/DataInitializer.java
@@ -6,6 +6,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import com.projeto.appspringthymeleaf.enums.TipoTelefoneEnum;
@@ -19,25 +21,28 @@ public class DataInitializer implements InitializingBean{
     @Autowired
     private UsuarioService usuarioService;
 
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+
     @Override
     public void afterPropertiesSet() throws Exception {
         UsuarioModel usuario = new UsuarioModel();
         List<TelefoneModel> telefones = new ArrayList<>();
 
-        usuario = new UsuarioModel(null, "João", "j@j.com", 25, LocalDate.now());
+        usuario = new UsuarioModel("João", "j@j.com", passwordEncoder.encode("123456"), 25, LocalDate.now());
 
         telefones.clear();
-        telefones.add( new TelefoneModel(null, "(11) 11111-1111", TipoTelefoneEnum.CELULAR, usuario) );
-        telefones.add( new TelefoneModel(null, "(11) 22222-2222", TipoTelefoneEnum.COMERCIAL, usuario) );
+        telefones.add( new TelefoneModel("(11) 11111-1111", TipoTelefoneEnum.CELULAR, usuario) );
+        telefones.add( new TelefoneModel("(11) 22222-2222", TipoTelefoneEnum.COMERCIAL, usuario) );
         usuario.setTelefones(telefones);
 
         usuarioService.save(usuario);
 
-        usuario = new UsuarioModel(null, "Maria", "m@m.com", 30, LocalDate.now());
+        usuario = new UsuarioModel("Maria", "m@m.com", passwordEncoder.encode("123456"), 30, LocalDate.now());
 
         telefones.clear();
-        telefones.add( new TelefoneModel(null, "(11) 33333-3333", TipoTelefoneEnum.RESIDENCIAL, usuario) );
-        telefones.add( new TelefoneModel(null, "(11) 44444-4444", TipoTelefoneEnum.CELULAR, usuario) );
+        telefones.add( new TelefoneModel("(11) 33333-3333", TipoTelefoneEnum.RESIDENCIAL, usuario) );
+        telefones.add( new TelefoneModel("(11) 44444-4444", TipoTelefoneEnum.CELULAR, usuario) );
         usuario.setTelefones(telefones);
 
         usuarioService.save(usuario);
