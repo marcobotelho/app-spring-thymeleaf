075726cd689448e940e3d1e1c49119ebd7fd16a7
Marco Botelho <marcobotelhoabreu@gmail.com>
2024-02-16 15:47:05 -0300

    Inclusão cadastro de roles user e admin para usuarios

--- src/main/java/com/projeto/appspringthymeleaf/util/DataInitializer.java
+++ src/main/java/com/projeto/appspringthymeleaf/util/DataInitializer.java
@@ -1,51 +1,53 @@
 package com.projeto.appspringthymeleaf.util;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import com.projeto.appspringthymeleaf.enums.TipoTelefoneEnum;
+import com.projeto.appspringthymeleaf.model.RoleModel;
 import com.projeto.appspringthymeleaf.model.TelefoneModel;
 import com.projeto.appspringthymeleaf.model.UsuarioModel;
+import com.projeto.appspringthymeleaf.service.RoleService;
 import com.projeto.appspringthymeleaf.service.UsuarioService;
 
 @Component
-public class DataInitializer implements InitializingBean{
+public class DataInitializer implements InitializingBean {
 
     @Autowired
     private UsuarioService usuarioService;
 
     @Autowired
-    private BCryptPasswordEncoder passwordEncoder;
+    private RoleService roleService;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @Override
     public void afterPropertiesSet() throws Exception {
-        UsuarioModel usuario = new UsuarioModel();
-        List<TelefoneModel> telefones = new ArrayList<>();
 
-        usuario = new UsuarioModel("João", "j@j.com", passwordEncoder.encode("123456"), 25, LocalDate.now());
+        RoleModel roleAdmin = new RoleModel("ROLE_ADMIN");
+        roleService.save(roleAdmin);
 
-        telefones.clear();
-        telefones.add( new TelefoneModel("(11) 11111-1111", TipoTelefoneEnum.CELULAR, usuario) );
-        telefones.add( new TelefoneModel("(11) 22222-2222", TipoTelefoneEnum.COMERCIAL, usuario) );
-        usuario.setTelefones(telefones);
+        RoleModel roleUser = new RoleModel("ROLE_USER");
+        roleService.save(roleUser);
 
-        usuarioService.save(usuario);
+        UsuarioModel usuario1 = new UsuarioModel("João", "j@j.com", passwordEncoder.encode("123456"), 25,
+                LocalDate.now());
+        usuario1.setRoles(List.of(roleAdmin, roleUser));
+        usuario1.setTelefones(List.of(new TelefoneModel("(11) 11111-1111", TipoTelefoneEnum.CELULAR, usuario1),
+                new TelefoneModel("(11) 22222-2222", TipoTelefoneEnum.COMERCIAL, usuario1)));
+        usuarioService.save(usuario1);
 
-        usuario = new UsuarioModel("Maria", "m@m.com", passwordEncoder.encode("123456"), 30, LocalDate.now());
-
-        telefones.clear();
-        telefones.add( new TelefoneModel("(11) 33333-3333", TipoTelefoneEnum.RESIDENCIAL, usuario) );
-        telefones.add( new TelefoneModel("(11) 44444-4444", TipoTelefoneEnum.CELULAR, usuario) );
-        usuario.setTelefones(telefones);
-
-        usuarioService.save(usuario);
+        UsuarioModel usuario2 = new UsuarioModel("Maria", "m@m.com", passwordEncoder.encode("123456"), 30,
+                LocalDate.now());
+        usuario2.setRoles(List.of(roleUser));
+        usuario2.setTelefones(List.of(new TelefoneModel("(11) 33333-3333", TipoTelefoneEnum.RESIDENCIAL, usuario2),
+                new TelefoneModel("(11) 44444-4444", TipoTelefoneEnum.CELULAR, usuario2)));
+        usuarioService.save(usuario2);
     }
-    
 }
