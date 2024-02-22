132d596f3768f819ce5cab2206f70a6ef0f56f80
Marco Botelho <marcobotelhoabreu@gmail.com>
2024-02-22 17:34:20 -0300

    Alteração carga inicial dados

--- src/main/java/com/projeto/appspringthymeleaf/util/DataInitializer.java
+++ src/main/java/com/projeto/appspringthymeleaf/util/DataInitializer.java
@@ -9,45 +9,66 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import com.projeto.appspringthymeleaf.enums.TipoTelefoneEnum;
-import com.projeto.appspringthymeleaf.model.RoleModel;
+import com.projeto.appspringthymeleaf.model.ClienteModel;
+import com.projeto.appspringthymeleaf.model.PerfilModel;
 import com.projeto.appspringthymeleaf.model.TelefoneModel;
 import com.projeto.appspringthymeleaf.model.UsuarioModel;
-import com.projeto.appspringthymeleaf.service.RoleService;
-import com.projeto.appspringthymeleaf.service.UsuarioService;
+import com.projeto.appspringthymeleaf.repository.ClienteRepository;
+import com.projeto.appspringthymeleaf.repository.PerfilRepository;
+import com.projeto.appspringthymeleaf.repository.UsuarioRepository;
+import com.projeto.appspringthymeleaf.service.CsvServiceSql;
 
 @Component
 public class DataInitializer implements InitializingBean {
 
-    @Autowired
-    private UsuarioService usuarioService;
+        @Autowired
+        private UsuarioRepository usuarioRepository;
 
-    @Autowired
-    private RoleService roleService;
+        @Autowired
+        private PerfilRepository perfilRepository;
 
-    @Autowired
-    private PasswordEncoder passwordEncoder;
+        @Autowired
+        private ClienteRepository clienteRepository;
 
-    @Override
-    public void afterPropertiesSet() throws Exception {
+        @Autowired
+        private PasswordEncoder passwordEncoder;
 
-        RoleModel roleAdmin = new RoleModel("ROLE_ADMIN");
-        roleService.save(roleAdmin);
+        @Autowired
+        private CsvServiceSql csvServiceSql;
 
-        RoleModel roleUser = new RoleModel("ROLE_USER");
-        roleService.save(roleUser);
+        @Override
+        public void afterPropertiesSet() throws Exception {
+                /* Criando os perfis */
+                PerfilModel perfilAdmin = new PerfilModel("ROLE_ADMIN", "Administrador");
+                perfilAdmin = perfilRepository.save(perfilAdmin);
+                PerfilModel perfilUser = new PerfilModel("ROLE_USER", "Usuário");
+                perfilUser = perfilRepository.save(perfilUser);
 
-        UsuarioModel usuario1 = new UsuarioModel("João", "j@j.com", passwordEncoder.encode("123456"), 25,
-                LocalDate.now());
-        usuario1.setRoles(List.of(roleAdmin, roleUser));
-        usuario1.setTelefones(List.of(new TelefoneModel("(11) 11111-1111", TipoTelefoneEnum.CELULAR, usuario1),
-                new TelefoneModel("(11) 22222-2222", TipoTelefoneEnum.COMERCIAL, usuario1)));
-        usuarioService.save(usuario1);
+                /* Criando os usuários */
+                UsuarioModel usuario1 = new UsuarioModel("Admin", "admin@email.com", passwordEncoder.encode("123"));
+                usuario1.setPerfis(List.of(perfilAdmin, perfilUser));
+                usuarioRepository.save(usuario1);
 
-        UsuarioModel usuario2 = new UsuarioModel("Maria", "m@m.com", passwordEncoder.encode("123456"), 30,
-                LocalDate.now());
-        usuario2.setRoles(List.of(roleUser));
-        usuario2.setTelefones(List.of(new TelefoneModel("(11) 33333-3333", TipoTelefoneEnum.RESIDENCIAL, usuario2),
-                new TelefoneModel("(11) 44444-4444", TipoTelefoneEnum.CELULAR, usuario2)));
-        usuarioService.save(usuario2);
-    }
+                UsuarioModel usuario2 = new UsuarioModel("User", "user@email.com", passwordEncoder.encode("123"));
+                usuario2.setPerfis(List.of(perfilUser));
+                usuarioRepository.save(usuario2);
+
+                /* Criando os clientes */
+                ClienteModel cliente1 = new ClienteModel("João", "j@j.com", 25,
+                                LocalDate.now(), "11111-111", "Rua 1 casa 1", "Centro", "São Paulo", "SP");
+                cliente1.setTelefones(List.of(new TelefoneModel("(11) 11111-1111", TipoTelefoneEnum.CELULAR, cliente1),
+                                new TelefoneModel("(11) 22222-2222", TipoTelefoneEnum.COMERCIAL, cliente1)));
+                clienteRepository.save(cliente1);
+
+                ClienteModel cliente2 = new ClienteModel("Maria", "m@m.com", 30,
+                                LocalDate.now(), "22222-222", "Rua 2 casa 2", "Centro", "São Paulo", "SP");
+                cliente2.setTelefones(List.of(
+                                new TelefoneModel("(11) 33333-3333", TipoTelefoneEnum.RESIDENCIAL, cliente2),
+                                new TelefoneModel("(11) 44444-4444", TipoTelefoneEnum.CELULAR, cliente2)));
+                clienteRepository.save(cliente2);
+
+                /* Criando estados e municipios dos arquivos CSV */
+                csvServiceSql.lerCsvEInserirDados();
+
+        }
 }
